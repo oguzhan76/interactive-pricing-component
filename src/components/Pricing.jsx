1b77c96c28f7c1bf21e7b2affa6b4c13bcb38e60
delucia <oguzhankukul@gmail.com>
2023-05-09 12:55:05 -0500

    toggle section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,32 +2,61 @@ import { useState, useRef } from "react";
 
 const Pricing = () => {
     const slider = useRef();
-    const [value, setValue] = useState();
-    const [pageviews, setPageviews] = useState();
-    const [payment, setPayment] = useState();
-    const [billing, setBilling] = useState('yearly');
-
-    const pricingElement = document.querySelector('.slider');
+    const [pageviews, setPageviews] = useState('100K');
+    const [payment, setPayment] = useState(16);
+    const [period, setPeriod] = useState('month');
 
     const handleOnChange = (e) => {
-        setValue(e.target.value)
-        pricingElement.style.setProperty('--lower-percent', `${e.target.value}%`);
-        pricingElement.style.setProperty('--upper-percent', `${100-e.target.value}%`);
+        switch(e.target.value) {
+            case '0':
+                setPageviews('10K');
+                setPayment(8);
+                break;
+            case '25':
+                setPageviews('50K');
+                setPayment(12);
+                break;
+            case '50':
+                setPageviews('100K');
+                setPayment(16);
+                break;
+            case '75':
+                setPageviews('500K');
+                setPayment(24);
+                break;
+            case '100':
+                setPageviews('1M');
+                setPayment(36);
+                break;
+        }
+
+        slider.current.style.setProperty('--range-value', `${e.target.value}%`);
     }
 
     return (
         <div className="pricing-container">
             <div className="pricing">
-                <div>
-                    <div>
+                <div className="offer-container">
+                    <div className="pageviews">
                         <p>{pageviews} PAGEVIEWS</p>
                     </div>
-                    <div>
-                        <p>$16.00<span>/month</span></p>
+                    <div className="payment">
+                        <p>{`$${payment}.00`}<span>/ {period}</span></p>
                     </div>
                 </div>
                 <div ref={slider} className="slider">
-                    <input  type="range" min='0' max='100' onChange={handleOnChange}/>
+                    <input  type="range" min='0' max='100' step="25" onChange={handleOnChange}/>
+                </div>
+                <div className="toggle-container">
+                    <p>Monthly Billing</p>
+                    <div className="toggle">
+                        <input type="checkbox" id="toggle"/>
+                        <label htmlFor="toggle"></label>
+                    </div>
+                    <p>Yearly Billing</p>
+                    <div className="discount">
+                        <p>25% discount</p>
+                    </div>
                 </div>
             </div>
         </div>
