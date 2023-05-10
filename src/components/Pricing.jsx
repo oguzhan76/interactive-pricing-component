c60e7bbc0f080b3d7483c5c6b90a16e616409d80
delucia <oguzhankukul@gmail.com>
2023-05-10 00:23:09 -0500

    finished

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,35 +1,24 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import plans from '../data/PaymentPlans.json';
+import icon_check from '/icon-check.svg';
 
 const Pricing = () => {
     const slider = useRef();
-    const [pageviews, setPageviews] = useState('100K');
-    const [payment, setPayment] = useState(16);
-    const [period, setPeriod] = useState('month');
+    const [currentPlan, setCurrentPlan] = useState(plans["3"]);
+    const [yearly, setYearly] = useState(false);
+    const [level, setLevel] = useState(3);
+
+    useEffect(() => {
+        // 25% discount for 12 months is 9 months
+        setCurrentPlan( {
+            ...plans[level],
+            payment: yearly ? plans[level].payment * 9 : plans[level].payment   
+        });
+    },[level, yearly]);
+    
 
     const handleOnChange = (e) => {
-        switch(e.target.value) {
-            case '0':
-                setPageviews('10K');
-                setPayment(8);
-                break;
-            case '25':
-                setPageviews('50K');
-                setPayment(12);
-                break;
-            case '50':
-                setPageviews('100K');
-                setPayment(16);
-                break;
-            case '75':
-                setPageviews('500K');
-                setPayment(24);
-                break;
-            case '100':
-                setPageviews('1M');
-                setPayment(36);
-                break;
-        }
-
+        setLevel(e.target.value / 25 + 1);
         slider.current.style.setProperty('--range-value', `${e.target.value}%`);
     }
 
@@ -38,10 +27,10 @@ const Pricing = () => {
             <div className="pricing">
                 <div className="offer-container">
                     <div className="pageviews">
-                        <p>{pageviews} PAGEVIEWS</p>
+                        <p>{currentPlan.pageviews} PAGEVIEWS</p>
                     </div>
                     <div className="payment">
-                        <p>{`$${payment}.00`}<span>/ {period}</span></p>
+                        <p>{`$${currentPlan.payment}.00`}<span>/ {yearly ? 'year' : 'month'}</span></p>
                     </div>
                 </div>
                 <div ref={slider} className="slider">
@@ -50,7 +39,7 @@ const Pricing = () => {
                 <div className="toggle-container">
                     <p>Monthly Billing</p>
                     <div className="toggle">
-                        <input type="checkbox" id="toggle"/>
+                        <input type="checkbox" id="toggle" onChange={() => setYearly(!yearly)}/>
                         <label htmlFor="toggle"></label>
                     </div>
                     <p>Yearly Billing</p>
@@ -58,6 +47,16 @@ const Pricing = () => {
                         <p>25% discount</p>
                     </div>
                 </div>
+                <footer className="pricing-footer">
+                    <section className="perks-section">
+                        <p><span><img src={icon_check} /></span>Unlimitied websites</p>
+                        <p><span><img src={icon_check} /></span>100% data ownership</p>
+                        <p><span><img src={icon_check} /></span>Email reports</p>
+                    </section>
+                    <section className="button-section">
+                        <button>Start my trial</button>
+                    </section>
+                </footer>
             </div>
         </div>
     )
